297aed4761a82b00f924e110a8323eb8de2ea16f
junhee <144101101+wnsjun@users.noreply.github.com>
2026-02-19 20:05:04 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.04 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,14 +1,13 @@
 def solution(array):
     count={}
-    
-    #1. 빈도수 세기
     for num in array:
         count[num]=count.get(num,0)+1
         
-    #2. 최댓값
-    max_count=max(count.values())
+    max_cnt=max(count.values())
     
-    #3. 최빈값 
-    modes = [num for num, cnt in count.items() if cnt == max_count]
+    modes = [num for num, cnt in count.items() if cnt == max_cnt]
         
-    return modes[0] if len(modes) == 1 else -1
+    return(
+        modes[0] 
+        if len(modes) == 1 else -1
+    ) 
