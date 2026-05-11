1b440acef68db3f5f2babeda40a1eda5d95a5cc9
junhee <144101101+wnsjun@users.noreply.github.com>
2026-05-11 22:58:01 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,13 +1,12 @@
 def solution(d, budget):
+    cnt=0
+    total=0
     d.sort()
-    count = 0
-    total = 0
-
+    
     for money in d:
-        total += money
-        
-        if total > budget:
+        total+=money
+        if total>budget:
             break
-        count += 1
-
-    return count
+        cnt+=1
+        
+    return cnt
