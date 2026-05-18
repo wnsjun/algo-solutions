99882bcd1aaeb39ca07fd07687c6ffcd176b5f85
junhee <144101101+wnsjun@users.noreply.github.com>
2026-05-18 22:32:43 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,9 +1,7 @@
 def solution(array, commands):
     answer = []
-    
     for i, j, k in commands:
-        sliced = array[i-1:j]   # i~j 자르기
-        sliced.sort()           # 정렬
-        answer.append(sliced[k-1])  # k번째 값 추가
-    
+        sliced= array[i-1:j]
+        sliced.sort()
+        answer.append(sliced[k-1])
     return answer
