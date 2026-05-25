cd9a97b2e7dd9271c612077dfaf7774be9374feb
junhee <144101101+wnsjun@users.noreply.github.com>
2026-05-25 22:26:13 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,16 +1,15 @@
 def solution(s):
-    result = ""
-    idx = 0
-
+    answer = ''
+    idx=0
+    
     for ch in s:
-        if ch == " ":
-            result += " "
-            idx = 0
+        if ch==" ":
+            answer+=" "
+            idx=0
         else:
-            if idx % 2 == 0:
-                result += ch.upper()
+            if idx%2==0:
+                answer+=ch.upper()
             else:
-                result += ch.lower()
-            idx += 1
-
-    return result
+                answer+=ch.lower()
+            idx+=1
+    return answer
