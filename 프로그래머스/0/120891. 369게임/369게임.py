f781198c774f8002aa5d25668c1a317069ce6f60
junhee <144101101+wnsjun@users.noreply.github.com>
2026-07-06 11:04:32 +0900

    [level 0] Title: 369게임, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.py
+++ 프로그래머스/0/120891. 369게임/369게임.py
@@ -1,9 +1,7 @@
 def solution(order):
     cnt =0
-    while (order):
-        a=order%10 #나머지
-        order=order//10 #몫
-        if a==3 or a==6 or a==9 :
+    for i in str(order):
+        if i=='3' or i=='6' or i=='9':
             cnt+=1
 
     return cnt
