4647195a82c506c0751688955a3262700439d525
junhee <144101101+wnsjun@users.noreply.github.com>
2026-08-12 22:18:23 +0900

    [level 1] Title: 기사단원의 무기, Time: 643.24 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,16 +1,18 @@
 def solution(number, limit, power):
+    sum=0
+    
     arr=[]
-    for i in range(1,number+1):
+    for i in range(1,number+1): #1부터 10까지 6
         cnt=0
-        for k in range(1,int(i**0.5)+1):  
-            if i%k==0:  
-                if i==k*k:
-                    cnt+=1
+        for k in range(1,int(i**0.5)+1): #1부터 3
+            if i%k==0: #k가 i의 약수면 
+                if i==k*k: #제곱수로 약수면 
+                    cnt+=1 #하나 더해
                 else:
-                    cnt+=2
-        if cnt<=limit:
-            arr.append(cnt)
+                    cnt+=2 #쌍이니까 두개 더해
+        if cnt>limit:
+            sum+=power
         else:
-            arr.append(power)
-            
-    return sum(arr)
+            sum+=cnt
+        
+    return sum
