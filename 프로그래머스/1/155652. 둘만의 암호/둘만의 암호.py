6dd6cd72ed814dd0c25ee22faae64aee81e2d0b1
junhee <144101101+wnsjun@users.noreply.github.com>
2026-08-14 14:31:08 +0900

    [level 1] Title: 둘만의 암호, Time: 0.11 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -0,0 +1,15 @@
+def solution(s, skip, index):
+    answer = ''
+
+    for i in s:
+        cnt = 0
+
+        while cnt < index:
+            i = chr((ord(i) - ord('a') + 1) % 26 + ord('a'))
+
+            if i not in skip:
+                cnt += 1
+
+        answer += i
+
+    return answer
